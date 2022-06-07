63f548b55ee2254efddd8788a67db1874f2f0b95
unknown <nemexxix@gmail.com>
2022-06-07 23:48:04 +0200

    fix usertype / register + modifypassword added

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationFormType;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use App\Services\FormService;
@@ -11,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,23 +24,26 @@ class UserController extends AbstractController
     private $formService;
     private $userRepository;
     private $serializeService;
+    private $mailer;
 
     public function __construct(
         UserPasswordHasherInterface  $passwordHasher, 
         FormService $formService,
         UserRepository $userRepository, 
-        SerializeService $serializeService
+        SerializeService $serializeService,
+        MailerInterface $mailer
     )
     {
         $this->passwordHasher = $passwordHasher;
         $this->formService = $formService;
         $this->userRepository = $userRepository;
         $this->serializeService = $serializeService;
+        $this->mailer = $mailer;
     }
 
 
     // Get the actual user
-    #[Route('/api/current-user', name: 'user', methods: ['GET', 'HEAD'])]
+    #[Route('/current-user', name: 'user', methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
         return $this->json($this->getUser());
@@ -48,19 +54,34 @@ class UserController extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $data["password"] = $this->passwordHasher->hashPassword($user, $data['password']);
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $genPassword = substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&'), 15, 15);
+        $data["password"] = $this->passwordHasher->hashPassword($user, $genPassword);
         $form->submit($data);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user, true);
+
+            $html = $this->renderView('user/emailBody.html.twig', [
+                'person' => $data,
+                'password' => $genPassword,
+            ]);
+
+            $email = (new Email())
+            ->from($_ENV['MailAdmin'])
+            ->to($_ENV['MailAdmin'])
+            ->subject('Inscription à la plateforme Ma ville')
+            ->html($html);
+
+            $this->mailer->send($email);
+
             return new Response($this->serializeService->SerializeGetLatest($this->userRepository));
         } else {
             return new JsonResponse($this->formService->getFormErrors($form), 400, ['Content-Type', 'application/json']);
         }
     }
 
-    #[Route('/edit/{id}', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    #[Route('/edit/{id}', name: 'app_user_edit', methods: ['GET', 'POST', 'PUT'])]
     public function edit(Request $request, User $user, UserRepository $userRepository): Response
     {
         $data = json_decode($request->getContent(), true);
@@ -72,15 +93,61 @@ class UserController extends AbstractController
 
             return new JsonResponse(['success' => true]);
         } else {
-            return new JsonResponse($this->formService->getFormErrors($form), 400, ['Content-Type', 'application/json']);
+            return new JsonResponse($user, 400, ['Content-Type', 'application/json']);
         }
     }
 
-    #[Route('/delete/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('/delete/{id}', name: 'app_user_delete', methods: ['POST', 'DELETE'])]
     public function delete(Request $request, User $user, UserRepository $userRepository): Response
     {
         $userRepository->remove($user, true);
         
         return new JsonResponse(['success' => true]);
     }
+
+        // Reset du mot de passe
+        #[Route('/api/users/modifyPassword/{id}', name: 'user_password_modify', methods: ['PUT'])]
+        public function passwordReset(Request $request, User $user, UserPasswordHasherInterface $passwordEncoder,  UserRepository $userRepository)
+        {
+            $data = json_decode($request->getContent(), true);
+            $form = $this->createForm(ModifyPasswordType::class, $user);
+            $form->submit($data);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+                if ($form->get('newPassword')->getData() === $form->get('confirmPassword')->getData()) {
+                    $password = $passwordEncoder->hashPassword($user, $form->get('newPassword')->getData());
+                    $user->setPassword($password);
+                    $userRepository->add($user, true);
+    
+                    $html = $this->renderView('user/emailConfirmPasswordChange.html.twig', [
+                        'person' => $user,
+                    ]);
+        
+                    $email = (new Email())
+                        ->from($_ENV['MailAdmin'])
+                        ->to($_ENV['MailAdmin'])
+                        ->subject('Changement de mot de passe sur la plateforme Replay')
+                        ->html($html);
+        
+        
+                    $this->mailer->send($email);
+    
+                    return new JsonResponse(['success' => true]);
+    
+                } else {
+                    return new JsonResponse(['newPassword' => 'les mots de passe doivent correspondre'], 400,
+                        ['Content-Type', 'application/json']);
+                }
+            } else {
+                if ($form->get('newPassword')->getData() != $form->get('confirmPassword')->getData()) {
+                    return new JsonResponse(
+                        [
+                            'oldPassword' => "Mot de passe actuel invalide",
+                            'newPassword' => 'les mots de passe doivent correspondre'
+                        ], 400, ['Content-Type', 'application/json']);
+                } else {
+                    return new JsonResponse($this->formService->getFormErrors($form), 400, ['Content-Type', 'application/json']);
+                }
+            }
+        }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,33 +3,35 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Entity\Formation;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname', Texttype::class)
-            ->add('firstname', TextType::class)
-            ->add('username', EmailType::class)
+            ->add('lastname')
+            ->add('firstname')
+            ->add('username')
             ->add('roles', ChoiceType::class, [
                 'choices'   => [
                     'ROLE_ADMIN'   => 'ROLE_ADMIN',
-                    'ROLE_USER'      => 'ROLE_USER'
+                    'ROLE_USER'      => 'ROLE_USER',
+                    'ROLE_FIRSTCONNECTION'=>'ROLE_FIRSTCONNECTION',
                 ],
                 'expanded' => true,
                 'multiple'  => true,
             ])
+            ->add('password')
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
